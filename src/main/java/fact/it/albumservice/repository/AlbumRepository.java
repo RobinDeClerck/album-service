a599cd7b1e007f2486f2b5c1a2ed42f1693a188d
Robin De Clerck <robin.de.clerck@gmail.com>
2022-01-02 18:43:15 +0100

    Create AlbumRepository.java

---
+++ src/main/java/fact/it/albumservice/repository/AlbumRepository.java
@@ -0,0 +1,8 @@
+package fact.it.albumservice.repository;
+
+import fact.it.albumservice.model.Album;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface AlbumRepository extends MongoRepository<Album, String> {
+    Album findArtistByName(String name);
+}
